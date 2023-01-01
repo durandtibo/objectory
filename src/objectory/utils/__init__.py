aca81b82e38c6efa0a1ea4e0bce6ca651034ae4f
Thibaut Durand <durand.tibo@gmail.com>
2023-01-01 14:16:08 -0800

    Add object helpers (#2)

---
+++ src/objectory/utils/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    "all_child_classes",
+    "full_object_name",
+    "import_object",
+    "instantiate_object",
+    "is_lambda_function",
+]
+
+from objectory.utils.object_helpers import (
+    all_child_classes,
+    full_object_name,
+    import_object,
+    instantiate_object,
+    is_lambda_function,
+)
