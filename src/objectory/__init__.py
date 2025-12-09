73ec4cc7a7dd19934d53e115752f5d488fa28468
Thibaut Durand <durand.tibo@gmail.com>
2025-12-09 08:53:35 -0800

    Improve build testing

--- src/objectory/__init__.py
+++ src/objectory/__init__.py
@@ -4,9 +4,15 @@ from __future__ import annotations
 
 __all__ = ["OBJECT_INIT", "OBJECT_TARGET", "AbstractFactory", "Registry", "__version__", "factory"]
 
+from importlib.metadata import PackageNotFoundError, version
+
 from objectory.abstract_factory import AbstractFactory
 from objectory.constants import OBJECT_INIT, OBJECT_TARGET
 from objectory.registry import Registry
 from objectory.universal import factory
 
-__version__ = "0.2.3a0"
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # Package is not installed, fallback if needed
+    __version__ = "0.0.0"
