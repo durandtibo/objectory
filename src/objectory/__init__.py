761dd307bb1d783b037dea35cbd7f0da645d67cd
Thibaut Durand <durand.tibo@gmail.com>
2025-12-09 08:39:36 -0800

    Improve build testing

--- src/objectory/__init__.py
+++ src/objectory/__init__.py
@@ -2,11 +2,11 @@ r"""Contain the main features of the ``objectory`` package."""
 
 from __future__ import annotations
 
-__all__ = ["OBJECT_INIT", "OBJECT_TARGET", "AbstractFactory", "Registry", "factory"]
+__all__ = ["OBJECT_INIT", "OBJECT_TARGET", "AbstractFactory", "Registry", "__version__", "factory"]
 
 from objectory.abstract_factory import AbstractFactory
 from objectory.constants import OBJECT_INIT, OBJECT_TARGET
 from objectory.registry import Registry
 from objectory.universal import factory
 
-__version__ = "0.2.3a0"  # placeholder for Hatchling to overwrite
+__version__ = "0.2.3a0"
