80d84b9a7ef2308bed4010bc2ce1271c1706109f
Thibaut Durand <durand.tibo@gmail.com>
2025-12-10 14:54:28 -0800

    Fix script to check package metadata

--- dev/package/check_metadata.sh
+++ dev/package/check_metadata.sh
@@ -6,14 +6,5 @@ METADATA=$(uv pip show objectory)
 
 echo "$METADATA"
 
-# Check if the package "objectory" is installed
-if ! echo "$METADATA" | grep -q "Name: objectory"; then
-    echo "Error: 'objectory' package is not installed or not found in pip metadata!" >&2
-    exit 1
-fi
-
-# Check if the package has no dependencies
-if ! echo "$METADATA" | grep -zq "Requires:\nRequired-by:"; then
-    echo "Error: 'objectory' package has dependencies listed." >&2
-    exit 1
-fi
+echo "$METADATA" | grep -zq "Name: objectory\n"
+echo "$METADATA" | grep -zq "Requires:\n"
