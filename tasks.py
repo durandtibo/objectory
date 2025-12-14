46d81b2b6f68651bb18b14dea734b0727b2d94e8
Thibaut Durand <durand.tibo@gmail.com>
2025-12-14 13:15:03 -0800

    Improve `tasks.py`

--- tasks.py
+++ tasks.py
@@ -296,7 +296,7 @@ def integration_test(c: Context, cov: bool = False) -> None:
     cmd = ["python -m pytest --xdoctest --timeout 60"]
     if cov:
         cmd.append(
-            f"--cov-report html --cov-report xml --cov-report term  --cov-append --cov={NAME}"
+            f"--cov-report html --cov-report xml --cov-report term --cov-append --cov={NAME}"
         )
         logger.info("📊 Coverage reports will be generated (appending)")
     cmd.append(f"{INTEGRATION_TESTS}")
